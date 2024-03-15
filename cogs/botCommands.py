990f40ff2e1d2ec9dc4294d38c62029ea0554f6b
Konstanty Walasek <kukibuki@gmail.com>
2024-03-15 14:33:45 +0100

    refactoring fixes

--- cogs/botCommands.py
+++ cogs/botCommands.py
@@ -42,31 +42,7 @@ class BotCommands(commands.Cog, name="BotCommands"):
         print("Restart request has been sent to bot")
         await context.send("Bot is restarting, wait a moment")
         restart_bot_script()
-    
-    # @tree.command(
-    #     name="restart_bot",
-    #     description="This restarts bot on controller server. Does not have any influence on the game servers",
-    #     guild=discord.Object(discord_server_id)
-    # )
-    # async def restart_bot(interaction):
-    #     print("Restart request has been sent to bot")
-    #     await interaction.response.send_message("Bot is restarting, wait a moment")
-    #     restart_bot_script()
-    
-    # @bot.event
-    # async def on_ready():
-    #     await tree.sync(guild=discord.Object(discord_server_id))
-    #     print(f'We have logged in as {bot.user}')
-    
-    # @bot.event
-    # async def on_message(message):
-    #     if message.author == bot.user:
-    #         return
-    
-    #     if message.content.startswith('$hello'):
-    #         await message.channel.send('Hello!')
-    
-    
+
     
     # And then we finally add the cog to the bot so that it can load, unload, reload and use it's content.
 async def setup(bot) -> None:
